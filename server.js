4d0bd5d27a7da1633169a2e2216867a72eda3e7a
BRECHT-PC\brecht <brecht.valcke@gentwebart.be>
2017-04-24 10:13:22 +0200

    order of controllers fix

--- server.js
+++ server.js
@@ -9,12 +9,14 @@ var controllers = require('./controllers');
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+
+
+controllers.set(app);
+
 app.use(express.static('public'));
 
 app.get("*",function(req,res){
   fs.createReadStream("./public/index.html").pipe(res);
 });
-
-controllers.set(app);
 
 app.listen(80);
