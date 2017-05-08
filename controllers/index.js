d226376121a360a2b9da066d30a4b51acac642e7
BRECHT-PC\brecht <brecht.valcke@gentwebart.be>
2017-05-08 16:05:05 +0200

    controllers removed

--- controllers/index.js
+++
@@ -1,7 +0,0 @@
-
-var serialport = require("./serial.js");
-var socketio = require("./socketio.js");
-
-module.exports.set = function(app,emitQeue,fs,settings){
-    serialport.set(app,fs,emitQeue,settings);
-};
--- controllers/serial.js
+++
@@ -1,5 +0,0 @@
-module.exports.set = function(app, fs,emitQueue,settings) {
-    // {"timestamp":{"hour":11,"min":12}}
-  
-
-};
